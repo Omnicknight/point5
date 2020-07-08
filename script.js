3d19e8e2440c039c9b1dc810a91e60580f44d8b4
Vlad Zobnin <vladopodobniy@gmail.com>
2020-07-09 02:11:30 +0300

    semifinal

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const product = 'tangerine';
+// const product = 'tangerine';
 
-function cook(product = 'lime') {
-    console.log('cook', product);
-}
+// function cook(product = 'lime') {
+//     console.log('cook', product);
+// }
 
-const random = function (min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+// const random = function (min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
 // cook();
 // function sum(a = 0, b = 0) {
 //     return a + b + random(a, b);
@@ -15,21 +15,47 @@ const random = function (min, max) {
 // console.log(sum(1, 2));
 
 
-function pow(n, p) {
-    let value = n;
-    if (p === 0){
-        return 1;
-    }
-    for (let i = 1; i < p; i++) {
-        n *= value;
-    }
-    return n;
+// function pow(n, p) {
+//     let value = n;
+//     if (p === 0){
+//         return 1;
+//     }
+//     for (let i = 1; i < p; i++) {
+//         n *= value;
+//     }
+//     return n;
+// }
+
+// console.log(pow(4, 5));
+
+// const sum = (a, b) => {
+//     return a + b;
+// };
+
+// const pow = a => pow(a, 2);
+
+function getRandom() {
+    return Math.random();
 }
+let number = 1;
 
-console.log(pow(4, 5));
-
-const sum = (a, b) => {
-    return a + b;
-};
-
-const pow = a => pow(a, 2);
+    while (number !== false) {
+    let random = Math.floor(getRandom()*10+1);
+    number = +prompt('Угадайте число от 1 до 10', '0');
+    if (isNaN(number)) {
+        console.log('введите число');
+    }
+    else if (number == false) {
+        console.log('Игра окончена');
+        break;
+    }
+    else if (number === random) {
+        console.log('Вы угадали');
+    }
+    else if (number) {
+        console.log('Вы не угадали');
+    }
+    // else {
+    //     console.log('Игра окончена');
+    // }
+}
